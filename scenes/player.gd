da6ef1e486fc5f77f536a823de7b57a2e887925c
Miguel <Miguel.Maldonado10@upr.edu>
2025-09-07 09:29:03 -0400

    Added Player Scrips & Tilemaps

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,1 +1,45 @@
 extends CharacterBody2D
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	#Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false # As good practice, you should replace UI actions with custom gameplay actions.true
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Play animation
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+			
+	# Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	elif is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+func die():
+	animated_sprite.flip_v
+	print("Player has been killed!")
+	set_process(false)
